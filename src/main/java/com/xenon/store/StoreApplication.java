e69d4513b275bab413abdfeecf44b341a8aea511
blessed-winner <blessedwinner66@gmail.com>
2026-01-14 15:22:13 +0200

    Facilitated entity creation with lombok

--- src/main/java/com/xenon/store/StoreApplication.java
+++ src/main/java/com/xenon/store/StoreApplication.java
@@ -1,14 +1,24 @@
 package com.xenon.store;
 
-import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.xenon.store.entities.User;
 
 
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
-	    SpringApplication.run(StoreApplication.class, args);
+	    //SpringApplication.run(StoreApplication.class, args);
+		//var user = new User(1L, "Winner", "blessedwinner66@gmail.com", "HomeAlone123");
+		//var name = user.getName();
+		//System.out.println("User name: " + name);
+
+		User.builder()
+		    .name("Winner")
+			.email("blessedwinner66@gmail.com")
+			.password("HomeAlone123");
      }
 
 }
